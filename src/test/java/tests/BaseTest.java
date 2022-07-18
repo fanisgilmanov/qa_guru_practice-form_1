45b4826dfa09d0df74ab0788a385e93439fe6cc2
FanisGilmanov <fanisgilmanov1994@gmail.com>
2022-07-18 21:07:53 +0500

    add setting selenoid and attach class

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,7 +1,12 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.RegistrationFormPage;
 
 public class BaseTest {
@@ -9,8 +14,25 @@ public class BaseTest {
     TestData testData = new TestData();
     @BeforeAll
     static void beforeUrl() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 
 }
--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -1,8 +1,9 @@
 package tests;
 
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Selenide.*;
-
+import static io.qameta.allure.Allure.step;
 
 
 public class PracticeFormTests  extends BaseTest{
@@ -10,37 +11,43 @@ public class PracticeFormTests  extends BaseTest{
 
     @Test
     void successfulTest() {
+        step("Открываем главную страницу", () -> {
+            registrationFormPage.openPage();
+        });
+        step("Вводим тестовые данные", () -> {
+            registrationFormPage.setFirstName(testData.firstName)
+                    .setLastName(testData.lastName)
+                    .setEmail(testData.userEmail)
+                    .setGender(testData.gender)
+                    .setMobileNumber(testData.userNumber)
+                    .setDateOfBirth(testData.dateBirthday)
+                    .setSubject(testData.subject)
+                    .setHobbies(testData.hobbies)
+                    .uploadFiles(testData.file)
+                    .setCurrentAddress(testData.currentAddress)
+                    .setSelect(testData.stateInput, testData.stateValue)
+                    .setSelect(testData.cityInput, testData.cityValue)
+                    .clickSubmit();
+        });
 
-        registrationFormPage.openPage()
-            .setFirstName(testData.firstName)
-            .setLastName(testData.lastName)
-            .setEmail(testData.userEmail)
-            .setGender(testData.gender)
-            .setMobileNumber(testData.userNumber)
-            .setDateOfBirth(testData.dateBirthday)
-            .setSubject(testData.subject)
-            .setHobbies(testData.hobbies)
-            .uploadFiles(testData.file)
-            .setCurrentAddress(testData.currentAddress)
-            .setSelect(testData.stateInput, testData.stateValue)
-            .setSelect(testData.cityInput, testData.cityValue)
-            .clickSubmit();
+
+
+        step("Проверяем введенные тестовые данные, на корректность", () -> {
+            registrationFormPage.checkResult("Student Name", testData.firstName)
+                    .checkResult("Student Name", testData.lastName)
+                    .checkResult("Student Email", testData.userEmail)
+                    .checkResult("Gender", testData.gender)
+                    .checkResult("Mobile", testData.userNumber)
+                    .checkResult("Date of Birth", testData.checkDateBirthday)
+                    .checkResult("Subjects", testData.subject)
+                    .checkResult("Hobbies", testData.hobbies)
+                    .checkResult("Picture", testData.file.getName())
+                    .checkResult("Address", testData.currentAddress)
+                    .checkResult("State and City", testData.stateValue + " " + testData.cityValue);
+        });
 
 
 
-
-
-         registrationFormPage.checkResult("Student Name", testData.firstName)
-            .checkResult("Student Name", testData.lastName)
-            .checkResult("Student Email", testData.userEmail)
-            .checkResult("Gender", testData.gender)
-            .checkResult("Mobile", testData.userNumber)
-            .checkResult("Date of Birth", testData.checkDateBirthday)
-            .checkResult("Subjects", testData.subject)
-            .checkResult("Hobbies", testData.hobbies)
-            .checkResult("Picture", testData.file.getName())
-            .checkResult("Address", testData.currentAddress)
-            .checkResult("State and City", testData.stateValue + " " + testData.cityValue);
 
     }
 }
