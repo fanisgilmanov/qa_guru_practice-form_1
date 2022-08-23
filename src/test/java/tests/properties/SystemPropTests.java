cd4d445abec2cc573d0abdca10650dbf5b8ca851
FanisGilmanov <fanisgilmanov1994@gmail.com>
2022-08-23 11:35:14 +0500

    add prop tests

---
+++ src/test/java/tests/properties/SystemPropTests.java
@@ -0,0 +1,22 @@
+package tests.properties;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class SystemPropTests {
+
+    @Test
+    @Tag("test1")
+    void SomeTest1() {
+        String browser = System.getProperty("browser");
+        String version = System.getProperty("version", "101");
+        String browserSize = System.getProperty("browserSize", "1920x1080");
+
+        System.out.println(browser); //
+        System.out.println(version); //
+        System.out.println(browserSize); //
+
+    }
+
+
+}
