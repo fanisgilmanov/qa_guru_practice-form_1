9eae3ab59f43b91fdff2e0a9e957ed5d6084014a
FanisGilmanov <fanisgilmanov1994@gmail.com>
2022-08-23 11:34:54 +0500

    add prop tests

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -24,6 +24,8 @@ public class BaseTest {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+
     }
 
 
